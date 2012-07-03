af268ab699ea32386c8e323a79d828140b9eabb0
Cristian Planas <me@cristianplanas.com>
2012-07-03 14:37:03 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FundidoANegro::Application.config.secret_token = 'fd9f2a1b9cd26ad0ef575f190867cee21327db06f4c48bbe15e8255716af21fb499d3de1bb793edce27009a20f62fe9c81087792f165d1b787151e9a6ecb5cf5'
