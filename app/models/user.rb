195fdb25c2a201d5991d5de4df4dfb7a6b595e22
Cristian Planas <me@cristianplanas.com>
2012-07-16 22:47:15 +0200

    No recoverable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   ROLES = %w{ editor admin }
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me,
     :role
