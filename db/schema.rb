3ee72565336b8d59c6f3b9dfc326662755d2e3f0
Cristian Planas <me@cristianplanas.com>
2012-07-17 00:50:22 +0200

    Adding types to document

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716221846) do
+ActiveRecord::Schema.define(:version => 20120716224204) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20120716221846) do
     t.integer  "author_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "type"
   end
 
   create_table "users", :force => true do |t|
