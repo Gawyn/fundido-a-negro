9868f8085d61d506b51a3b2fe8a6ba08f7b21760
Cristian Planas <me@cristianplanas.com>
2012-07-17 00:25:01 +0200

    Document table created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716185858) do
+ActiveRecord::Schema.define(:version => 20120716221846) do
+
+  create_table "documents", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "author_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
