00a475c5125c2790a700cb8ecde009671af31dbe
Cristian Planas <me@cristianplanas.com>
2012-07-18 00:29:49 +0200

    Defining new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 FundidoANegro::Application.routes.draw do
+  devise_for :users, :controllers => { :sessions => "backoffice/sessions" }, :path_prefix => "/backoffice"
   namespace :backoffice do
-    devise_for :users, :controllers => { :sessions => "backoffice/sessions" }
     resources :users, :except => [:show]
+    resources :documents, :only => [:index, :destroy]
+    resources :articles, :except => [:index, :show, :destroy]
+    resources :reviews, :except => [:index, :show, :destroy]
   end
+
+  resources :articles, :only => [:show]
 
   root :to => "home#index"
 end
