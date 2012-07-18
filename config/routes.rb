77a11188adaf670e07f679408801664b6aeb0b84
Cristian Planas <me@cristianplanas.com>
2012-07-18 16:13:34 +0200

    Routes for the new resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ FundidoANegro::Application.routes.draw do
   devise_for :users, :controllers => { :sessions => "backoffice/sessions" }, :path_prefix => "/backoffice"
   namespace :backoffice do
     resources :users, :except => [:show]
-    resources :documents, :only => [:index, :destroy]
-    resources :articles, :except => [:index, :show, :destroy]
-    resources :reviews, :except => [:index, :show, :destroy]
+    resources :articles, :except => [:show]
+    resources :reviews, :except => [:show]
   end
 
   resources :articles, :only => [:show]
