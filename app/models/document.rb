42d7bf36507156738fb8a308b13d0d1c514100fe
Cristian Planas <me@cristianplanas.com>
2012-07-30 01:44:10 +0200

    Adding status column to document table

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,9 +1,12 @@
 class Document < ActiveRecord::Base
   TYPES = %w{ Article Review }
+  STATUS = %w{ cabecera portada }
 
-  attr_accessible :body, :title, :author
+  attr_accessible :body, :title, :author,
+    :status
 
   belongs_to :author, :class_name => "User"
 
   validates_inclusion_of :type, :in => TYPES
+  validates_inclusion_of :status, :in => STATUS, :allow_blank => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716224204) do
+ActiveRecord::Schema.define(:version => 20120729231259) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120716224204) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "type"
+    t.string   "status"
   end
 
   create_table "users", :force => true do |t|
