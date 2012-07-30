0a2bcdd1173df490c4c0dccdf8270521807cbe2a
Cristian Planas <me@cristianplanas.com>
2012-07-30 21:47:04 +0200

    Adding column author to document table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730193910) do
+ActiveRecord::Schema.define(:version => 20120730194239) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120730193910) do
     t.datetime "updated_at",    :null => false
     t.string   "type"
     t.string   "status"
+    t.string   "author"
   end
 
   create_table "users", :force => true do |t|
