6061276d275f9cc86130c0dc1f75c616aa3e2888
Cristian Planas <me@cristianplanas.com>
2012-07-30 21:41:03 +0200

    CreatedBy to CreatedById

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120729234552) do
+ActiveRecord::Schema.define(:version => 20120730193910) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "created_by"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "created_by_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
     t.string   "type"
     t.string   "status"
   end
