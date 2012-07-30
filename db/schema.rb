baae9303cfd0a30d1040f672754909042771a7a1
Cristian Planas <me@cristianplanas.com>
2012-07-30 23:53:54 +0200

    Adding HeaderImage to Document table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730194239) do
+ActiveRecord::Schema.define(:version => 20120730211614) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20120730194239) do
     t.string   "type"
     t.string   "status"
     t.string   "author"
+    t.string   "header_image"
   end
 
   create_table "users", :force => true do |t|
