ef3d35907335bcfca7f3ab6e335146649dc1aa53
Cristian Planas <me@cristianplanas.com>
2012-07-31 02:50:42 +0200

    Big bug with author_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
     :role
 
-  has_many :documents, :foreign_key => "author_id"
-  has_many :articles, :foreign_key => "author_id"
-  has_many :reviews, :foreign_key => "author_id"
+  has_many :documents, :foreign_key => "created_by_id"
+  has_many :articles, :foreign_key => "created_by_id"
+  has_many :reviews, :foreign_key => "created_by_id"
 
-  ROLES.each do |role|
+  ROLES.each do |role_name|
     define_method "#{role_name}?" do
       role == role_name
     end
