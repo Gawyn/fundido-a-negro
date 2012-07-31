6d2203be4166f7d718e19a372389e4f1092da459
Cristian Planas <me@cristianplanas.com>
2012-07-31 02:48:21 +0200

    defining methods for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
   has_many :documents, :foreign_key => "author_id"
   has_many :articles, :foreign_key => "author_id"
   has_many :reviews, :foreign_key => "author_id"
+
+  ROLES.each do |role|
+    define_method "#{role_name}?" do
+      role == role_name
+    end
+  end
 end
