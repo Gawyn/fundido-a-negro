9e65945c5e56395745de0f399f347ea4bdf10b0a
Cristian Planas <me@cristianplanas.com>
2012-07-31 02:15:18 +0200

    Adding poster column to document table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730211614) do
+ActiveRecord::Schema.define(:version => 20120731000506) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120730211614) do
     t.string   "status"
     t.string   "author"
     t.string   "header_image"
+    t.string   "poster"
   end
 
   create_table "users", :force => true do |t|
