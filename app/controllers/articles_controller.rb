eb8693d168ea91d22ad53494efa0b6b76288750d
Cristian Planas <me@cristianplanas.com>
2012-07-31 02:38:09 +0200

    Not using CanCan in public part of the web

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
   inherit_resources
-  load_resource
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,12 @@
 class ReviewsController < ApplicationController
   inherit_resources
   load_resource
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
 end
