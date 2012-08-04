44ff3ee2a2bd7e17d1f8c97cc008c7bcda71127b
Cristian Planas <me@cristianplanas.com>
2012-08-04 20:33:05 +0200

    published attribute for model document

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,7 @@ class Document < ActiveRecord::Base
   STATUS = %w{ cabecera portada }
 
   attr_accessible :body, :title, :created_by,
-    :status, :author, :header_image
+    :status, :author, :header_image, :published
 
   belongs_to :created_by, :class_name => "User"
 
