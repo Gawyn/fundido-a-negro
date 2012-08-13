821f4bc6c580efe4f5d0c89fe5031674185e9b02
Cristian Planas <me@cristianplanas.com>
2012-08-13 21:29:07 +0200

    scope published

--- app/models/document.rb
+++ app/models/document.rb
@@ -10,6 +10,8 @@ class Document < ActiveRecord::Base
   validates_inclusion_of :type, :in => TYPES
   validates_inclusion_of :status, :in => STATUS, :allow_blank => true
 
+  scope :published, where(arel_table[:published].eq(true))
+
   mount_uploader :header_image, HeaderImageUploader
 
   def formatted_body
